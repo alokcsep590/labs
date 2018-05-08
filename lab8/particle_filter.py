6d223c1b93d4f4884ff8473247a85cfe73657b04
Alok Jain <alokjain@microsoft.com>
2018-05-08 14:36:06 -0700

    Some more experiments for convergence.

--- lab8/particle_filter.py
+++ lab8/particle_filter.py
@@ -1,5 +1,7 @@
 import math
-import numpy
+import numpy as np
+import copy
+import numpy.random as npr
 
 from grid import *
 from particle import Particle
@@ -7,6 +9,61 @@ from utils import *
 from setting import *
 from scipy.stats import norm
 
+def multinomial_resample(weights):
+    cumulative_sum = np.cumsum(weights)
+
+    cumulative_sum[-1] = 1.  # avoid round-off errors: ensures sum is exactly one
+
+    return np.searchsorted(cumulative_sum, npr.random(len(weights)))
+    
+def systematic_resample(weights):
+    N = len(weights)
+
+    # make N subdivisions, choose positions 
+    # with a consistent random offset
+    positions = (np.arange(N) + random.random()) / N
+
+    indexes = np.zeros(N, 'i')
+    cumulative_sum = np.cumsum(weights)
+    i, j = 0, 0
+    while i < N:
+        if positions[i] < cumulative_sum[j]:
+            indexes[i] = j
+            i += 1
+        else:
+            j += 1
+    return indexes
+
+def stratified_resample(weights):
+    N = len(weights)
+
+    # make N subdivisions, and chose a random position within each one
+
+    positions = (npr.random(N) + range(N)) / N
+
+
+
+    indexes = np.zeros(N, 'i')
+
+    cumulative_sum = np.cumsum(weights)
+
+    i, j = 0, 0
+
+    while i < N:
+
+        if positions[i] < cumulative_sum[j]:
+
+            indexes[i] = j
+
+            i += 1
+
+        else:
+
+            j += 1
+
+    return indexes
+
+    
 def motion_update(particles, odom):
     """ Particle filter motion update
 
@@ -59,24 +116,60 @@ def measurement_update(particles, measured_marker_list, grid):
         Returns: the list of particles represents belief p(x_{t} | u_{t})
                 after measurement update
     """
+    
+    #print("particles=")
     #print(particles)
-    weight_particles = [1/len(particles)]*len(particles)
+
+    weight_particles = [1.0/len(particles)]*len(particles)
     for idx, particle in enumerate(particles):
-        particle_marker_list = particle.read_markers(grid)
-        rmarker_weight = 1.0
-        for rmarker in measured_marker_list[:]:
-            # pdf(x, loc=0, scale=1) is proabability x with mean as loc and variance as scale
-            # https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.stats.norm.html
-            pmarker_weight = 0.0
-            for pmarker in particle_marker_list[:]: 
-                pmarker_weight += norm.pdf(pmarker[0],rmarker[0],MARKER_TRANS_SIGMA)*norm.pdf(pmarker[1],rmarker[1],MARKER_TRANS_SIGMA)*norm.pdf(pmarker[2],rmarker[2],MARKER_ROT_SIGMA)            
-            rmarker_weight *= pmarker_weight            
-        weight_particles[idx] += rmarker_weight
-        
+        if  grid.is_in(particle.x, particle.y):
+            found = False
+            particle_marker_list = particle.read_markers(grid)
+            rmarker_weight = 1.0
+            for rmarker in measured_marker_list[:]:
+                # pdf(x, loc=0, scale=1) is proabability x with mean as loc and variance as scale
+                # https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.stats.norm.html
+                pmarker_weight = 1.e-300
+                for pmarker in particle_marker_list[:]: 
+                    pmarker_weight += len(particles)*norm.pdf(pmarker[0],rmarker[0],MARKER_TRANS_SIGMA)*norm.pdf(pmarker[1],rmarker[1],MARKER_TRANS_SIGMA)*norm.pdf(pmarker[2]%360,rmarker[2],MARKER_ROT_SIGMA)            
+                    found = True
+                rmarker_weight *= pmarker_weight
+    
+            if found:
+                weight_particles[idx] += rmarker_weight            
+
+            """    
+            if (len(measured_marker_list)==0 and len(particle_marker_list) ==0):
+                None
+            elif (len(measured_marker_list)==len(particle_marker_list)):
+                weight_particles[idx] += 10*rmarker_weight
+            elif weight_particles[idx] > rmarker_weight:
+                weight_particles[idx] -= 10*rmarker_weight
+            else:
+                weight_particles[idx] = 1.e-300          
+            """
+        else:
+            weight_particles[idx] = 1.e-300
+    
+    #print("weight_particles=")
+    #print(weight_particles)    
     norm_weight_particles = [float(i)/sum(weight_particles) for i in weight_particles]
 
-    measured_particles = numpy.random.choice(particles, len(particles), p = norm_weight_particles)
+    #measured_particles = np.random.choice(copy.deepcopy(particles), len(particles), p = norm_weight_particles)
     
+    #print("norm_weight_particles=")
+    #print(norm_weight_particles)
+    
+    
+    indexes = stratified_resample(norm_weight_particles)
+    #print("indexes")
+    #print(indexes)
+    measured_particles = []
+    for index in indexes[:]:
+        measured_particles.append(copy.deepcopy(particles[index]))
+    
+    
+    #print("measured_particles=")
     #print(measured_particles)
-
+    
     return measured_particles
